cf880cb0aa84f5194df992a20728f2885b4a2a0b
Jordan Gary <JigSays@gmail.com>
2018-01-30 19:41:46 -0600

    fixes random generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,74 +13,76 @@ var targetScore;
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
  }
- 
- var result = getRndInteger(19, 120);
- console.log(result);
-
 //creates an enviroment for starting/re-starting the game
-play = function(){
-
-targetScore = function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+function play (){
+    playerScore = 0;
+    var targetScore = getRndInteger(19, 120);
+    var crystalOneVal = getRndInteger(1, 13);
+    var crystalTwoVal = getRndInteger(1, 13);
+    var crystalThreeVal = getRndInteger(1, 13);
+    var crystalFourVal = getRndInteger(1, 13);
+    console.log("target", targetScore);
+    console.log("crystal1:", crystalOneVal);
+    console.log("crystal2:", crystalTwoVal);
+    console.log("crystal3:", crystalThreeVal);
+    console.log("crystal4:", crystalFourVal);
 }
 
-crystalOneVal = function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-crystalTwoVal = function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-crystalThreeVal = function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-crystalFourVal = function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-targetScore(19, 120);
-crystalOneVal(1, 13);
-crystalTwoVal(1, 13);
-crystalThreeVal(1, 13);
-crystalFourVal(1, 13);
-playerScore = 0;
-console.log(targetScore);
-}
+play();
 
 //sets eventlisteners
-$("#c1").click(function(){
+var c1 = $("#c1");
+var c2 = $("#c2");
+var c3 = $("#c3");
+var c4 = $("#c4");
+
+c1.click(addPts);
+    function addPts() {
+       $(crystalOneVal) += playerScore; 
+    }
+console.log("player score:", playerScore);
+$("#c1").click(){
     $(crystalOneVal) += playerScore;
+    render();
 });
 
-$("#c2").click(function(){
+$("#c2").click(addPts) {
     $(crystalTwoVal) += playerScore;
-});
+    rendr();
+}
 
 $("#c3").click(function(){
     $(crystalThreeVal) += playerScore;
+    render();
 });
 
 $("#c4").click(function(){
     $(crystalFourVal) += playerScore;
+    render();
 });
 
 //Displays to dom
-$("#winBox").innerText += wins;
-$("#lossBox").innerText += losses;
-$("#tagetScore").innerText = targetScore;
-$("#yourBox").innerText = playerScore;
+function render(){
+    var newP = $("#scoreBox");
+    winBox = $("<p>").text("Wins: " + wins);
+    winBox.append(newP);
+    $("#lossBox").html(losses);
+    $("#tagetScore").text(targetScore);
+    $("#yourBox").text(" " + playerScore);
+}
+render();
 
 //advances scoreboard for win/loss and resets the game.
 if (playerScore === targetScore) {
-    wins +=1;
-    play();
+    wins++;
+    render();
+    then.play();
 }
 
-if (playerScore > targetScore){
-    losses +=1;
-    play();
+if (playerScore > targetScore) {
+    losses++;
+    render();
+    then.play();
 }
 
 
